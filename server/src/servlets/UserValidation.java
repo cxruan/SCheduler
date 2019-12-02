00fc76b513fdbab4229a539e3034adffbc43cc6e
Yichi Yang <yichiyan@usc.edu>
2019-12-01 21:49:14 -0800

    Fix UserValidation.java

--- server/src/servlets/UserValidation.java
+++ server/src/servlets/UserValidation.java
@@ -23,54 +23,38 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @WebServlet("/api/login")
-public class UserValidation extends HttpServlet 
-{
+public class UserValidation extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-	{
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		HttpSession session = request.getSession();
-		
-		boolean next = true;
+
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
-		
-		if(username.isEmpty())
-		{
+
+		if (username.isEmpty()) {
 			response.getWriter().append(new JsonResponse("error", "Username cannot be blank.").toJson());
 			return;
 		}
-		
-		if(password.isEmpty()&&next)
-		{
+
+		if (password.isEmpty()) {
 			response.getWriter().append(new JsonResponse("error", "Password cannot be blank.").toJson());
 			return;
 		}
-		
+
 		boolean ValidUser = DatabaseManager.validateUsername(username);
-		
-		if(ValidUser)
-		{
-			response.getWriter().append(new JsonResponse("ok", null).toJson());
-			session.setAttribute("username", username);
-			return;
+
+		if (ValidUser) {
+			Credential cr = DatabaseManager.getHashAndSalt(username);
+			boolean Validpassword = cr.getHash().equals(Util.sha256Digest(password + cr.getSalt()));
+
+			if (Validpassword) {
+				response.getWriter().append(new JsonResponse("ok", null).toJson());
+				session.setAttribute("username", username);
+				return;
+			}
 		}
 		response.getWriter().append(new JsonResponse("error", "Username and password does not match").toJson());
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
